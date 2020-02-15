b118cf5ca2583d3bef57a9fccd0c574c50d8738d
surfmast3r <surfmast3r@live.it>
2020-02-15 13:20:53 +0100

    accommodation detail view created and navigation fixed

--- app/src/main/java/com/surfmaster/consigliaviaggi/ui/accommodation/AccommodationFragment.java
+++ app/src/main/java/com/surfmaster/consigliaviaggi/ui/accommodation/AccommodationFragment.java
@@ -6,14 +6,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import com.squareup.picasso.Picasso;
 import com.surfmaster.consigliaviaggi.R;
 
 public class AccommodationFragment extends Fragment {
@@ -30,6 +37,9 @@ public class AccommodationFragment extends Fragment {
         accommodationViewModel =
                 ViewModelProviders.of(this).get(AccommodationViewModel.class);
         View root = inflater.inflate(R.layout.fragment_accommodation_detail, container, false);
+
+        initToolbar(root);
+
         final TextView textView = root.findViewById(R.id.text_accommodation_datail);
         accommodationViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -37,16 +47,26 @@ public class AccommodationFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        ImageView accommodationImage = root.findViewById(R.id.collapsing_ac_image);
+        Picasso.get().load("https://www.oasidellapizza.it/wp-content/uploads/revslider/steweysfullslider/5.jpg").into(accommodationImage);
         return root;
+    }
+
+    private void initToolbar(View root) {
+        Toolbar toolbar = root.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        NavController navController = Navigation.findNavController(((AppCompatActivity)getActivity()), R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(((AppCompatActivity)getActivity()), navController);
     }
 
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         super.onPrepareOptionsMenu(menu);
-        MenuItem menuItem = menu.findItem(R.id.action_filter);
+        MenuItem menuItem = menu.findItem(R.id.action_settings);
         menuItem.setVisible(false);
     }
-
 
 
 }
